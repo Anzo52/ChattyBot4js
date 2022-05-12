558afd150f40c5e5b009377f13133c7ba98c1aa7
Anzo52 <azollner2014@gmail.com>
2022-05-12 12:31:34 -0600

    Create ChattyBot.js

---
+++ ChattyBot.js
@@ -0,0 +1,10 @@
+const input = require('sync-input');
+
+console.log("Hello! My name is Aid.");
+console.log("I was created in 2022.");
+console.log("Please, remind me your name.");
+
+// use input() + to get the user's output
+let yourName = input()
+
+console.log("What a great name you have, " + yourName + "!");
